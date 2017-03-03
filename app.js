5a56edc9499100aea9b02707d50de7ece36867ce
HONGJS <hongjs@kr.ibm.com>
2017-03-03 17:20:25 +0900

    updated

--- app.js
+++ app.js
@@ -21,8 +21,49 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+//console.log(appEnv.getServiceCreds('Redis Cloud-lb'));
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+var redis = require("redis");
+var redisCredentials = appEnv.getServiceCreds('Redis Cloud-lb');
+//var client=redis.createClient(credentials.uri);
+
+var redisClient = redis.createClient({
+    'host': redisCredentials.hostname,
+    'port': redisCredentials.port,
+    'password': redisCredentials.password
+});
+
+redisClient.on('error', function (err) {
+    console.log('error event - ' + redisClient.host + ':' + redisClient.port + ' - ' + err);
+});
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.put("/hset", function(request, response) {
+	var key = "myhash";
+	redisClient.hset(key, request.body.field, request.body.value, function(error, result) {
+		if (error) {
+			response.status(500).send(error);
+		} else {
+			response.send(result == 1 ? "created":"updated");
+		}
+    });
+});
+
+app.get("/hget", function(request, response) {
+	var key = "myhash";
+	redisClient.hget(key, request.query.field, function(error, result) {
+		if (error) {
+			response.status(500).send(error);
+		} else {
+			response.send(result);
+		}
+    });
 });
